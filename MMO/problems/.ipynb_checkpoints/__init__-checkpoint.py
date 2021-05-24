1d6e1672a8bb74ef25b93faa68c8377f61e26008
laloc <laloc2496@gmail.com>
2021-05-24 08:43:25 +0700

    update CDN placement

--- MMO/problems/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from MMO.problems.problem import Problem
-from MMO.problems.zdt import ZDT1, ZDT2, ZDT3
-from MMO.problems.dtlz import DTLZ1, DTLZ2, DTLZ3, DTLZ4, DTLZ5, DTLZ6
-from MMO.problems.oka import OKA1, OKA2
-from MMO.problems.vlmop import VLMOP2, VLMOP3
-from MMO.problems.re import RE1, RE2, RE3, RE4, RE5, RE6, RE7
-from MMO.problems.cdn import CDN_RAM, CDN_PLACEMENT
